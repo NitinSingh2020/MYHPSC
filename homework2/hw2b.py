2bfa5e18021ee1c743a31004cea4580d09aa0ec6
Nitin Singh <nitin.singh2020@gmail.com>
2014-04-10 09:31:05 -0400

    Added part 5 and 6 to hw2b.py

--- homework2/hw2b.py
+++ homework2/hw2b.py
@@ -56,6 +56,14 @@ def plot_quad(xi,yi):
     Plots the interpolating polynomial & data points
     and save the fig as quadratic.png
     """
+    # check inputs and print error message if not valid:
+
+    error_message = "xi and yi should have type numpy.ndarray"
+    assert (type(xi) is np.ndarray) and (type(yi) is np.ndarray), error_message
+
+    error_message = "xi and yi should have length 3"
+    assert len(xi)==3 and len(yi)==3, error_message
+
     # Get c, an array containing the coefficients of
     # p(x) = c[0] + c[1]*x + c[2]*x**2.
     c = quad_interp(xi,yi)
@@ -80,10 +88,91 @@ def test_quad2():
     """
     Test code, no return value or exception if test runs properly.
     """
-    xi = np.array([-1.,  0.,  2.])
-    yi = np.array([ 1., -1.,  7.])
-    plot_quad(xi,yi)
+    xi = np.array([-2.,  1.,  5.])
+    yi = np.array([ 7., 1.,  49.])
+    c = quad_interp(xi,yi)
+    c_true = np.array([-1.,  0.,  2.])
+    print "c =      ", c
+    print "c_true = ", c_true
+    # test that all elements have small error:
+    assert np.allclose(c, c_true), \
+        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
+
+def cubic_interp(xi,yi):
+    """
+    Cubic interpolation.  Compute the coefficients of the polynomial
+    interpolating the points (xi[i],yi[i]) for i = 0,1,2.
+    Returns c, an array containing the coefficients of
+      p(x) = c[0] + c[1]*x + c[2]*x**2 + c[3]*x^3.
+
+    """
+
+    # check inputs and print error message if not valid:
+
+    error_message = "xi and yi should have type numpy.ndarray"
+    assert (type(xi) is np.ndarray) and (type(yi) is np.ndarray), error_message
+
+    error_message = "xi and yi should have length 4"
+    assert len(xi)==4 and len(yi)==4, error_message
+
+    # Set up linear system to interpolate through data points:
+    A = np.vstack([np.ones(4), xi, xi**2, xi**3]).T
+    b = yi
+    
+    ### Fill in this part to compute c ###
+    c = solve(A,b)
+
+    return c
+
+def plot_cubic(xi,yi):
+    """
+    Plots the interpolating polynomial & data points
+    and save the fig as quadratic.png
+    """
+    # check inputs and print error message if not valid:
+
+    error_message = "xi and yi should have type numpy.ndarray"
+    assert (type(xi) is np.ndarray) and (type(yi) is np.ndarray), error_message
+
+    error_message = "xi and yi should have length 4"
+    assert len(xi)==4 and len(yi)==4, error_message
+    
+    # Get c, an array containing the coefficients of
+    # p(x) = c[0] + c[1]*x + c[2]*x**2 + c[3]*x^3.
+    c = cubic_interp(xi,yi)
+
+    # Plot the resulting polynomial:
+    x = np.linspace(xi.min() - 1,  xi.max() + 1, 1000) # points to evaluate polynomial
+    y = c[0] + c[1]*x + c[2]*x**2 + c[3]*x**3
+
+    plt.figure(1)       # open plot figure window
+    plt.clf()           # clear figure
+    plt.plot(x,y,'b-')  # connect points with a blue line
+
+    # Add data points  (polynomial should go through these points!)
+    plt.plot(xi,yi,'ro')   # plot as red circles
+    plt.ylim(-2,100)         # set limits in y for plot
+
+    plt.title("Data points and interpolating polynomial")
+
+    plt.savefig('cubic.png')   # save figure as .png file
+    
+def test_cubic1():
+    """
+    Test code, no return value or exception if test runs properly.
+    """
+    xi = np.array([-1.,  0.,  2., 4.])
+    yi = np.array([ 0., -1.,  15., 95.])    
+    c = cubic_interp(xi,yi)
+    c_true = np.array([-1.,  0.,  2., 1.])
+    print "c =      ", c
+    print "c_true = ", c_true
+    # test that all elements have small error:
+    assert np.allclose(c, c_true), \
+        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
+    plot_cubic(xi,yi)
     print "png file saved in the current working directory"
+
     
 if __name__=="__main__":
     # "main program"
@@ -95,4 +184,5 @@ if __name__=="__main__":
     print "Running test..."
     test_quad1()
     test_quad2()
+    test_cubic1()
 
